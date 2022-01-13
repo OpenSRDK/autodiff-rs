c624764aa29d255c25981c5d80c78cd48380f222
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2022-01-13 22:30:03 +0900

    feat: logarithm compaction

--- src/expression/transcendental_expression/functions/ln.rs
+++ src/expression/transcendental_expression/functions/ln.rs
@@ -1,9 +1,27 @@
 use crate::{Expression, TranscendentalExpression};
+use num_traits::ToPrimitive;
 
 impl Expression {
     pub fn ln(self) -> Self {
         if let Expression::Constant(v) = self {
             return Expression::Constant(v.ln());
+        }
+        if let Expression::Mul(l, r) = &self {
+            return l.as_ref().clone().ln() + r.as_ref().clone().ln();
+        }
+        if let Expression::Pow(base, exponent) = &self {
+            return exponent.to_f64().unwrap_or_default() * base.as_ref().clone().ln();
+        }
+        if let Expression::Transcendental(v) = &self {
+            match v.as_ref() {
+                TranscendentalExpression::Pow(base, exponent) => {
+                    return exponent.as_ref().clone() * base.as_ref().clone().ln();
+                }
+                TranscendentalExpression::Exp(arg) => {
+                    return arg.as_ref().clone().into();
+                }
+                _ => {}
+            }
         }
 
         TranscendentalExpression::Ln(self.into()).into()
--- src/expression/transcendental_expression/functions/log.rs
+++ src/expression/transcendental_expression/functions/log.rs
@@ -1,10 +1,27 @@
 use crate::{Expression, TranscendentalExpression};
+use num_traits::ToPrimitive;
 
 impl Expression {
     pub fn log(self, antilogarithm: Expression) -> Self {
         if let Expression::Constant(base) = self {
             if let Expression::Constant(antilogarithm) = antilogarithm {
                 return Expression::Constant(antilogarithm.log(base));
+            }
+        }
+        if let Expression::Mul(l, r) = &self {
+            return l.as_ref().clone().log(antilogarithm.clone())
+                + r.as_ref().clone().log(antilogarithm);
+        }
+        if let Expression::Pow(base, exponent) = &self {
+            return exponent.to_f64().unwrap_or_default()
+                * base.as_ref().clone().log(antilogarithm);
+        }
+        if let Expression::Transcendental(v) = &self {
+            match v.as_ref() {
+                TranscendentalExpression::Pow(base, exponent) => {
+                    return exponent.as_ref().clone() * base.as_ref().clone().log(antilogarithm);
+                }
+                _ => {}
             }
         }
 
