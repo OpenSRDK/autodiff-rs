8519e0c3f306da2b255704bb77ce2256abadfec1
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-02-12 23:02:47 +0800

    fix: test

--- tests/tensor_test.rs
+++ tests/tensor_test.rs
@@ -1,6 +1,4 @@
-use std::collections::HashMap;
-
-use opensrdk_symbolic_computation::{new_symbol_tensor, Expression, Size};
+use opensrdk_symbolic_computation::{new_symbol_tensor, Size};
 
 extern crate blas_src;
 extern crate lapack_src;
@@ -20,8 +18,7 @@ fn test_main() {
     let pdf_expression = (-0.5
         * ((x.clone() - mu.clone())
             .inner_prod(precision, &[[0, 0]])
-            .inner_prod(x.clone() - mu.clone(), &[[1, 0]]))
-        .as_degenerated_tensor_scalar())
+            .inner_prod(x.clone() - mu.clone(), &[[1, 0]])))
     .exp();
 
     let diff = pdf_expression.differential(&["x", "mu", "lambda"]);
@@ -46,12 +43,9 @@ fn test_main2() {
 
     let pdf_expression = (-0.5
         * ((x.clone() - mu.clone())
-            .to_mat()
             .t()
-            .as_tensor()
             .inner_prod(precision, &[[1, 0]])
-            .inner_prod(x.clone() - mu.clone(), &[[1, 0]]))
-        .as_degenerated_tensor_scalar())
+            .inner_prod(x.clone() - mu.clone(), &[[1, 0]])))
     .exp();
 
     let diff = pdf_expression.differential(&["x", "mu", "lambda"]);
@@ -65,43 +59,4 @@ fn test_main2() {
     println!("{:#?}", diff[1].tex_code(&tex_symbols));
     println!("sigma diff");
     println!("{:#?}", diff[2].tex_code(&tex_symbols));
-}
-
-trait Distribution<T> {
-    fn ln_pdf(&self, value: &T, condition: &HashMap<String, Expression>) -> Expression;
-
-    fn pdf(&self, value: &T, condition: &HashMap<String, Expression>) -> Expression {
-        self.ln_pdf(value, condition).exp()
-    }
-}
-
-struct MultivariateNormal {
-    x: String,
-    mu: String,
-    sigma: String,
-}
-
-impl MultivariateNormal {
-    pub fn new(x: String, mu: String, sigma: String) -> MultivariateNormal {
-        MultivariateNormal { x, mu, sigma }
-    }
-}
-
-impl Distribution<Vec<f64>> for MultivariateNormal {
-    fn ln_pdf(&self, value: &Vec<f64>, condition: &HashMap<String, Expression>) -> Expression {
-        let x = &condition[&self.x.clone()];
-        let mu = condition[&self.mu.clone()];
-        let sigma = &condition[&self.sigma.clone()];
-
-        let pdf_expression = (-0.5
-            * ((x.clone() - mu.clone())
-                .inner_prod(precision, &[[0, 0]])
-                .inner_prod(x.clone() - mu.clone(), &[[1, 0]]))
-            .as_scalar())
-        .exp();
-
-        let res: Result<usize, usize> = Ok(0);
-
-        res.pdf_expression
-    }
 }
