a6a80e78ec6233f8502088e7f21b555801e078b6
Rubens <91782701+tibetsou@users.noreply.github.com>
2023-02-20 23:05:47 +0900

    feat: create test of sub, neg

--- src/expression/operators/neg.rs
+++ src/expression/operators/neg.rs
@@ -29,3 +29,44 @@ impl Expression {
         format!("{{-{}}}", v._tex_code(symbols, BracketsLevel::ForOperation))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        collections::HashMap,
+        ops::{Add, Neg},
+    };
+
+    use opensrdk_linear_algebra::sparse::SparseTensor;
+
+    use crate::Expression;
+
+    #[test]
+    fn it_works() {
+        let a1 = 5.0f64;
+        let b1 = vec![a1; 8];
+        let mut hash1 = HashMap::new();
+        hash1.insert(vec![3, 2, 1], 2.0);
+        hash1.insert(vec![1usize; 3], 3.0);
+        hash1.insert(vec![4usize; 3], 4.0);
+        hash1.insert(vec![5usize; 3], 2.0);
+        let c1 = SparseTensor::from(vec![6usize; 3], hash1).unwrap();
+
+        let ea1 = Expression::from(a1);
+        let eb1 = Expression::from(b1.clone());
+        let ec1 = Expression::from(c1.clone());
+
+        let ea = ea1.neg();
+        let eb = eb1.neg();
+        let ec = ec1.neg();
+
+        let a = Expression::from(-a1);
+        let b = Expression::from(b1.iter().map(|j| -j).collect::<Vec<f64>>());
+        //let c = Expression::from(-c1);
+
+        assert_eq!(ea, a);
+        assert_eq!(eb, b);
+        //assert_eq!(ec, c);
+        println!("{:?}", ec);
+    }
+}
