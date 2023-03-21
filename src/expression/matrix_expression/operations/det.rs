44b49281ac5751a2dc76989683eb7787bdcb72d3
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-03-21 23:34:16 +0900

    feat: det

--- src/expression/matrix_expression/operations/det.rs
+++ src/expression/matrix_expression/operations/det.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use opensrdk_linear_algebra::Matrix;
 
-use crate::{BracketsLevel, ConstantValue, Expression, MatrixExpression};
+use crate::{new_variable_tensor, BracketsLevel, ConstantValue, Expression, MatrixExpression};
 
 impl Expression {
     pub fn det(self) -> Expression {
@@ -38,5 +38,40 @@ impl MatrixExpression {
             r"\left\|{}\right\|",
             v._tex_code(symbols, BracketsLevel::None)
         )
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::new_variable_tensor;
+    #[test]
+    fn it_works() {
+        use crate::Size;
+
+        let x = new_variable_tensor("x".to_string(), vec![Size::Many, Size::Many]);
+        let mu = new_variable_tensor("mu".to_string(), vec![Size::Many, Size::Many]);
+        let sigma = new_variable_tensor("sigma".to_string(), vec![Size::Many, Size::Many]);
+        let expression = x * mu / sigma;
+        let diff_x = expression.differential(&["x"])[0].clone();
+        let diff_mu = expression.differential(&["mu"])[0].clone();
+        let diff_sigma = expression.differential(&["sigma"])[0].clone();
+        let diff_anpan = expression.differential(&["anpan"])[0].clone();
+
+        let tex_symbols: Vec<_> = vec![("x", "x"), ("mu", r"\mu"), ("sigma", r"\Sigma")]
+            .into_iter()
+            .collect();
+        let tex_symbols: HashMap<_, _> = tex_symbols.into_iter().collect();
+
+        assert_eq!(expression.tex_code(&tex_symbols), r"\frac{x \mu}{\Sigma}");
+        assert_eq!(diff_x.tex_code(&tex_symbols), r"\frac{\mu}{\Sigma}");
+        assert_eq!(diff_mu.tex_code(&tex_symbols), r"\frac{x}{\Sigma}");
+        assert_eq!(
+            diff_sigma.tex_code(&tex_symbols),
+            r"\frac{-x \mu}{\Sigma^2}"
+        );
+        assert_eq!(diff_anpan.tex_code(&tex_symbols), r"\frac{0 \mu}{\Sigma}");
     }
 }
