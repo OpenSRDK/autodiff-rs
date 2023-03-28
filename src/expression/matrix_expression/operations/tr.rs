b6406d1b5b130c9425a47aa8b06e6ce16da0e63d
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-03-28 22:37:06 +0900

    feat: test for tr

--- src/expression/matrix_expression/operations/tr.rs
+++ src/expression/matrix_expression/operations/tr.rs
@@ -11,3 +11,23 @@ impl Expression {
         tensor
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use opensrdk_linear_algebra::Matrix;
+
+    use crate::Expression;
+    #[test]
+    fn it_works() {
+        fn it_works() {
+            let len = 3usize;
+            let a = Matrix::from(len, vec![1.0, 3.0, 4.0, 0.0, 1.0, 0.0, 0.0, 0.0, 3.0]).unwrap();
+            let ea = Expression::from(a.clone());
+
+            let tr_a = a.clone().tr();
+            let tr_ea = ea.clone().tr();
+
+            assert_eq!(Expression::from(tr_a), tr_ea);
+        }
+    }
+}
