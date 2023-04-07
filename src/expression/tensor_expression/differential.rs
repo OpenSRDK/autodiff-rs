f56d05c69be12f7ae96fd156dfb8750bf17012c9
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-04-07 22:10:49 +0900

    feat: tensor_diff

--- src/expression/tensor_expression/differential.rs
+++ src/expression/tensor_expression/differential.rs
@@ -17,20 +17,33 @@ impl TensorExpression {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
+#[cfg(test)]
+mod tests {
+    use opensrdk_linear_algebra::generate_rank_combinations;
 
-//     use crate::TensorExpression;
+    use crate::expression::tensor_expression::TensorExpression;
+    use crate::{new_variable_tensor, Size};
 
-//     #[test]
-//     fn differential() {
-//         let x = TensorExpression::KroneckerDeltas(vec![[1, 1], [2, 2]]);
-//         let y = TensorExpression::KroneckerDeltas(vec![[1, 1], [2, 2]]);
-//         let z = TensorExpression::KroneckerDeltas(vec![[1, 1], [2, 2]]);
-//         let x = x.dot(y, &[[0, 0], [1, 1]]).dot(z, &[[0, 0], [1, 1]]);
+    #[test]
+    fn it_works() {
+        let id = "x";
+        let ex = new_variable_tensor((id).to_string(), vec![Size::Many, Size::Many]);
 
-//         let diff = x.differential(&["x"]);
-//         println!("diff:{:?}", diff);
-//         assert_eq!(diff.len(), 1);
-//     }
-// }
+        let k = TensorExpression::KroneckerDeltas(vec![[1, 1], [2, 2]]);
+        let k_diff = k.differential(&[id]);
+        assert_eq!(k_diff, vec![0.0.into(); 1]);
+
+        let rank_combination = generate_rank_combinations(&[[2, 2]]);
+
+        let dop = TensorExpression::DotProduct {
+            terms: vec![ex.clone(), ex.clone()],
+            rank_combinations: rank_combination.to_vec(),
+        };
+        let dp_diff = dop.differential(&[id]);
+        println!("{:?}", dp_diff);
+
+        let dip = TensorExpression::DirectProduct(vec![ex.clone(), ex.clone()]);
+        let dip_diff = dip.differential(&[id]);
+        println!("{:?}", dip_diff);
+    }
+}
