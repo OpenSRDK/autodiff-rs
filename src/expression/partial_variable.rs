814aa3461edec96e8126cf1c59cd1edaeed9f23c
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-04-14 22:53:52 +0900

    feat: test partial variable

--- src/expression/partial_variable.rs
+++ src/expression/partial_variable.rs
@@ -1,5 +1,4 @@
 use crate::{Expression, ExpressionArray};
-use std::collections::HashMap;
 
 pub fn new_partial_variable(v: ExpressionArray) -> Expression {
     Expression::PartialVariable(v)
@@ -19,5 +18,34 @@ impl Expression {
                 ))
             })
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{new_partial_variable, new_variable_tensor, ExpressionArray, Size};
+
+    #[test]
+    fn diff_partial_variable() {
+        let x = new_variable_tensor("x".to_string(), vec![Size::Many, Size::Many]);
+        let y = new_variable_tensor("y".to_string(), vec![Size::Many, Size::Many]);
+        let z = new_variable_tensor("z".to_string(), vec![Size::Many, Size::Many]);
+
+        let mut a = ExpressionArray::new(vec![1, 3]);
+        a[&[0, 0]] = x;
+        a[&[0, 1]] = y;
+        a[&[0, 2]] = z;
+
+        let v = new_partial_variable(a);
+        let diff_x = v.differential(&["x"]);
+        println!("diff_x: {:?}", diff_x);
+        let diff_y = v.differential(&["y"]);
+        println!("diff_y: {:?}", diff_y);
+        let diff_z = v.differential(&["z"]);
+        println!("diff_z: {:?}", diff_z);
+
+        assert_eq!(diff_x.len(), 1);
+        assert_eq!(diff_y.len(), 1);
+        assert_eq!(diff_z.len(), 1);
     }
 }
